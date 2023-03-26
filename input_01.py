819952573d0ccab4813a67ad808851738892257a
Deliarr <philoeso31@gmail.com>
2023-03-26 16:20:39 +0200

    modification input_01

--- input_01.py
+++ input_01.py
@@ -2,7 +2,6 @@
 #
 #########################################
 
-
 # Ici on cré un variable annee qui est = input ("une question") qui est un int entier
 annee = input("Saisissez une année : ")
 annee = int(annee)
@@ -12,7 +11,7 @@ if annee % 400 == 0 or (annee % 4 == 0 and annee %100 != 0):
 else:
     print("L'année saisie n'est pas une année bissextile.")
     
-# Ici on a cré une
+# Ici on demande le nom et on affiche le nom
 def demander_nom():
     nom = input("Quel est votre nom: ")
     return nom
@@ -20,7 +19,7 @@ def demander_nom():
 nom_personne = demander_nom()
 print(nom_personne)
 
-
+# Ici on pose une question ou l'on demande le nom, mais si la personne ne rentre rien, il y a le message Erreur: le nom ne doit pas être vide
 def demander_nom():
     nom = input("Quel est votre nom: ")
     if nom == "":
